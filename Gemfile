468c4a945605fee6d04d1b0966ad653e0c12ae15
kpinar <kpinar@seas.upenn.edu>
2015-06-09 00:04:58 +0200

    report controller and paperclip

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'omniauth'
 gem 'devise'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.1'
+gem "paperclip", "~> 4.2"
 # Use postgresql as the database for Active Record
 gem 'pg'
 # Use SCSS for stylesheets
---
+++ app/controllers/report_controller.rb
@@ -0,0 +1,14 @@
+class ReportController < ApplicationController
+	def create
+ 	 @report = Report.create(report_params)
+	end
+
+	private
+
+	# Use strong_parameters for attribute whitelisting
+	# Be sure to update your create() and update() controller methods.
+
+	def user_params
+  		params.require(:report).permit(:avatar)
+	end
+end
