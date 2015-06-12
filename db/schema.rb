1c301dfc9d529c6ca9792ba15e678cfbfddb02fa
kpinar <kpinar@seas.upenn.edu>
2015-06-12 12:44:12 +0200

    trying to resolve user profile merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150611113446) do
+ActiveRecord::Schema.define(version: 20150611205919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150611113446) do
     t.string   "address"
     t.string   "report_type"
     t.string   "time"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
