5cbcc02aea6290c16836a09d189828011d322e8a
Alex Schwartz <aischwa@umich.edu>
2015-06-12 15:46:28 +0200

    started making funny seeds for presentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,7 @@ Report.delete_all
 Report.create! report_type: "Theft", description: "Ian stole my heart", address: "1600 Pennsylvania Avenue Northwest, Washington, DC 20500, United States", latitude: "38.8977", longitude: "77.0366"
 Report.create! report_type: "Harassment", description: "Ian always asks if I need help", address: "1 World Trade Center, New York, NY 10007, United States", latitude: "40.7117", longitude: "74.0125"
 Report.create! report_type: "Vandalism", description: "Ian broke my stand", address: "100 Washington Ave, St. Louis, MO 63102, United States", latitude: "38.6245", longitude: "90.1847"
-Report.create! report_type: "Stalking", description: "Santa keeps following us to the bars", address: "73 Kloof St, Gardens, Cape Town, 8001, South Africa", latitude: "33.93035", longitude: "18.41077"
+Report.create! report_type: "Stalking", description: "Santa keeps following us places", address: "73 Kloof St, Gardens, Cape Town, 8001, South Africa", latitude: "33.93035", longitude: "18.41077"
+Report.create! report_type: "Assault", description: "Kate pushed Pavi down the stairs for taking her headphones", address: "19 Park Road, Gardens, Cape Town, 8001, South Africa", latitude: "33.92863", longitude: "18.41033"
+Report.create! report_type: "Sexual Assault", description: "Mariella tickled Ian's elbow lovingly without permission", address: "51 Kloof Street, Gardens, Cape Town, 8001, South Africa", latitude: "33.9291", longitude: "18.41176"
+
